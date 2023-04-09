c970f6717ff34a4803ccd1788e97fde47c8a612c
DespaiiR <102087726+DespaiiR@users.noreply.github.com>
2023-04-09 15:40:51 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,10 @@
 var burger = function () {
     let activeBurger = document.getElementsByClassName('header__burger')[0];
     let activeNav = document.querySelector('.header__nav');
-    /*activeBurger.addEventListener('click', function() {
+    activeBurger.addEventListener('click', function() {
         activeBurger.classList.toggle('active');
         activeNav.classList.toggle('active');
-    });*/
-    activeBurger.onclick = function () {
-        activeBurger.classList.toggle('active');
-        activeNav.classList.toggle('active');
-    }
+    });
 }
 burger();
 
