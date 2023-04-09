7b3dcb7fa1262353cc8b50cba0d7918355a68448
DespaiiR <102087726+DespaiiR@users.noreply.github.com>
2023-04-09 14:44:44 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,41 @@
+let burger = function () {
+    let activeBurger = document.querySelector('.header__burger');
+    let activeNav = document.querySelector('.header__nav');
+    activeBurger.onclick = function () {
+        activeBurger.classList.toggle('active');
+        activeNav.classList.toggle('active');
+    }
+}
+burger();
+
+let select = function () {
+    let selectHeader = document.querySelectorAll('.form-select__header');
+    let selectItem = document.querySelectorAll('.form-select__item')
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+    function selectToggle() {
+        this.parentElement.classList.toggle('form-select_focus');
+    }
+    function selectChoose() {
+        let text = this.innerText;
+        let select = this.closest('.form-select')
+        let currentText = select.querySelector('.form-select__current');
+        currentText.innerText = text;
+        select.classList.remove('form-select_focus')
+    }
+}
+select();
+
+let range = function () {
+    let rangeInput = document.querySelector('.range__input');
+    let rangeValue = document.querySelector('.range__header-value');
+    rangeValue.innerHTML = rangeInput.value;
+    rangeInput.oninput = function() {
+        rangeValue.innerHTML = this.value;
+    } 
+}
+range()
